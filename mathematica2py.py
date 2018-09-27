033dbaa561e5a841c9f6d0116d56ad3ce281c083
HJK <hjjimmykim@gmail.com>
2018-09-27 11:04:08 -0400

    a0->p1[0]

--- mathematica2py.py
+++ mathematica2py.py
@@ -5,6 +5,18 @@ def mathematica2py(input = 'V.txt'):
 
     text = ''.join(text.split()) # Remove whitespace
     text = text.replace('^','**') # Replace ^ with **
+    
+    text = text.replace('a0','p1[0]')
+    text = text.replace('a1','p1[1]')
+    text = text.replace('a2','p1[2]')
+    text = text.replace('a3','p1[3]')
+    text = text.replace('a4','p1[4]')
+    
+    text = text.replace('b0','p2[0]')
+    text = text.replace('b1','p2[1]')
+    text = text.replace('b2','p2[2]')
+    text = text.replace('b3','p2[3]')
+    text = text.replace('b4','p2[4]')
 
     num_terms = text.count('{')
 
