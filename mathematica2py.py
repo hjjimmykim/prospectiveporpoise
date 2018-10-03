52d59f476e88dda92de0d64cab90a3a01609c2a4
HJK <hjjimmykim@gmail.com>
2018-10-03 19:37:34 -0400

    Mathematica

--- mathematica2py.py
+++ mathematica2py.py
@@ -6,6 +6,7 @@ def mathematica2py(input = 'V.txt', ID_me=1, ID_otherguy=2):
     text = ''.join(text.split()) # Remove whitespace
     text = text.replace('^','**') # Replace ^ with **
     
+    '''
     text = text.replace('a0','p' + str(ID_me) + '[0]')
     text = text.replace('a1','p' + str(ID_me) + '[1]')
     text = text.replace('a2','p' + str(ID_me) + '[2]')
@@ -17,6 +18,7 @@ def mathematica2py(input = 'V.txt', ID_me=1, ID_otherguy=2):
     text = text.replace('b2','p' + str(ID_otherguy) + '[2]')
     text = text.replace('b3','p' + str(ID_otherguy) + '[3]')
     text = text.replace('b4','p' + str(ID_otherguy) + '[4]')
+    '''
 
     num_terms = text.count('{')
 
